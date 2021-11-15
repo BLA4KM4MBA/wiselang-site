dc1d30bbaf5271ca8607a0ddbb4a2e1dfbb9bad5
Abhi <81672229+Hatred0001@users.noreply.github.com>
2021-11-15 15:20:11 +0530

    positioning commit

--- js/darkmode.js
+++ js/darkmode.js
@@ -3,7 +3,7 @@ const options = {
   right: '32px', // default: '32px'
   left: 'unset', // default: 'unset'
   time: '0.5s', // default: '0.3s'
-  mixColor: '#fff', // default: '#fff'
+  Color: '#000', // default: '#fff'
   backgroundColor: '#fff',  // default: '#fff'
   buttonColorDark: '#100f2c',  // default: '#100f2c'
   buttonColorLight: '#fff', // default: '#fff'
